52344de7d29cfe81d14ba08d8d93c750e5afe8a3
Jakob Skjerning <jakob@mentalized.net>
2016-02-15 11:45:44 +0100

    Adds support for running remote rake tasks

--- lib/capistrano/remote/runner.rb
+++ lib/capistrano/remote/runner.rb
@@ -7,6 +7,12 @@ module Capistrano
         @host = host
       end
 
+      def rake(task)
+        remote_command = "cd #{current_path} && #{rake_command(task)}"
+        local_command = "ssh -l #{user} #{hostname} -t \"#{remote_command}\""
+        exec local_command
+      end
+
       def run_interactively(command)
         remote_command = "cd #{current_path} && #{rails_command(command)}"
         local_command = "ssh -l #{user} #{hostname} -t \"#{remote_command}\""
@@ -25,6 +31,11 @@ module Capistrano
 
       def rails_command(command)
         "#{bundle_command} exec rails #{command}"
+      end
+
+      def rake_command(arguments)
+        command = fetch(:rake_command, SSHKit.config.command_map[:rake])
+        "#{command} #{arguments}"
       end
 
       def user
